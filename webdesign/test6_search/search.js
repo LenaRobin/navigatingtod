255905e3e8798ce68d7073e8a2c982eda1d3c80a
jbatin <exjosipbatinic@091.050.092.145.hva.nl>
2015-10-23 15:52:37 +0200

    search cleanup

--- webdesign/test6_search/search.js
+++ webdesign/test6_search/search.js
@@ -1,5 +1,30 @@
 $(document).ready(function(){
 
+    function addLink() {
+        //Get the selected text and append the extra info
+        var selection = window.getSelection(),
+            pagelink = '<br /><br /> Read more at: ' + document.location.href,
+            copytext = selection + pagelink,
+            newdiv = document.createElement('div');
+
+        //hide the newly created container
+        newdiv.style.position = 'absolute';
+        newdiv.style.left = '-99999px';
+
+        //insert the container, fill it with the extended text, and define the new selection
+        document.body.appendChild(newdiv);
+        newdiv.innerHTML = copytext;
+        selection.selectAllChildren(newdiv);
+
+        window.setTimeout(function () {
+            document.body.removeChild(newdiv);
+        }, 100);
+    }
+
+    document.addEventListener('copy', addLink);
+
+
+
 	var elem = $('<div id="search_ui" style="position: fixed; right: 10px; top: 10px; display: none;"><button id="cancel">Cancel</button></div>');
 
 	$('body').append(elem);
