8a299cb140ee228014368fe135ffab29f450ed66
jbatin <exjosipbatinic@Josip-Batinics-MacBook-Pro.local>
2015-11-03 09:38:20 +0100

    messing with keywords

--- webdesign/test_keywords/keywords.js
+++ webdesign/test_keywords/keywords.js
@@ -1,10 +1,12 @@
 $(document).ready(function(){
-	$(".keyword").click(function() {
+	$(".in-text").click(function() {
 		var info = $(this).attr('id');
 		var top = $(this).offset().top;
 		var left = $(this).offset().left;
-		$(".tip").html(info).css("display","block").offset({top: top - 30, left: left });
-
+		$("full-ref").html(info).css({
+			display: block,
+			border: 1px solid black,
+		}).offset({top: top - 30, left: left });
 		console.log($(this).offset().top);
 	});
 
