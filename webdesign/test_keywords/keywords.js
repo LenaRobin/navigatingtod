76d929620bf1ef1fc1bd9a0f400c0d8537951596
jbatin <exjosipbatinic@031.185.028.145.hva.nl>
2015-11-03 17:32:33 +0100

    in-text ref

--- webdesign/test_keywords/keywords.js
+++ webdesign/test_keywords/keywords.js
@@ -1,13 +1,15 @@
 $(document).ready(function(){
 	$(".in-text").click(function() {
-		var info = $(this).attr('id');
+		$(document).tooltip();
+
+/*		//var info = $(this).attr('id');
+		var info = $(this).data("ref");
+		console.log("info : " +info);
+		//html-> <p data-ref="XXX">
 		var top = $(this).offset().top;
 		var left = $(this).offset().left;
-		$("full-ref").html(info).css({
-			display: block,
-			border: 1px solid black,
-		}).offset({top: top - 30, left: left });
-		console.log($(this).offset().top);
+		//$(".full-ref").html(info).offset({top: top - 30, left: left });
+		console.log($(this).offset().top);*/
 	});
 
 });
