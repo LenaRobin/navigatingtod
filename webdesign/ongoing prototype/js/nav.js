1fda15770363515ba360f524c4e29271fa0d7d9f
LenaRobin <lena.robin1@gmail.com>
2015-11-10 16:13:21 +0100

    print style + nav.js

--- webdesign/ongoing prototype/js/nav.js
+++ webdesign/ongoing prototype/js/nav.js
@@ -1,17 +1,45 @@
 var toppyDiv = 0;
 var sValues = [];
+var counter = 0;
 
 $('.section').each(function () {
     var scrValue = $(this).offset().top;
+    console.log(scrValue);
     sValues.push(scrValue);
     $(this).attr("data-scroll", scrValue);
-    console.log(scrValue);
+    //console.log(scrValue);
 });
+
+$(".nav-tabs h2 a").each(function(ind){
+    $(this).attr("id", "hid"+(ind+1)); 
+    $(this).attr("data-scr", sValues[ind]);
+});
+
+$(".nav-tabs h2 a").on('click', function(e){
+    e.preventDefault();
+    var number = $(this).attr("data-scr");
+    var index = $(this).attr("id");
+    scrollIt(number, index);
+});
+
+function scrollIt(number, index){
+    $('html, body').animate({
+    //    scrollTop: number;}, 'slow');
+            scrollTop: number })    
+    }
 
 $("#display").on('click', function (e){
     e.preventDefault();
+    if(counter >= sValues.length - 1){
+  //is the last
+    $(this).css("display","none");
+  }else{
+      $(this).css("display","block");
+  }
+   // $(this).css("display","block");
       if($('.current').next('div.section').length > 0) {
-            console.log('here '+$('.current').next('div.section').length);
+            //console.log('here '+$('.current').next('div.section').length);
+           
             var $next = $('.current').next('.section');
             var top = $next.offset().top ;
 
@@ -21,27 +49,40 @@ $("#display").on('click', function (e){
                 $('html, body').animate({
                     scrollTop: top
                 });
-                console.log('hey');
+                //console.log('hey');
             });
+          
         }else{
-         //console.log("nothing");  
-            var $next = $('.section:first');
-            var top = $next.offset().top ;
-            console.log($('#info-box').height());
+        // console.log("no next"); 
+            //console.log("offset "+$('.text').offset().top);
+            //console.log(sValues[0]);
+            if(counter == 0){
+                //is the first
+                var $next = $('.section:first');
+                var top = $next.offset().top ;
+                //console.log($('#info-box').height());
 
-            //$('.current').removeClass('current');
-            $(function () {
-                $next.addClass('current toppy');
-                $('html, body').animate({
-                    scrollTop: top
+                //$('.current').removeClass('current');
+                $(function () {
+                    $next.addClass('current toppy');
+                    $('html, body').animate({
+                        scrollTop: top
+                    });
+                    //console.log();
                 });
-                //console.log();
-            });
+                
+            }//else{
+                
+           // }
+           // 
+            
         }
+    counter++;
 });
 
 $("#display1").on('click', function (e){
     e.preventDefault();
+    $("#display").css("display","block");
     if( $('.current').prev('.section').length){
         var $prev = $('.current').prev('.section');
         var top = $prev.offset().top;
@@ -60,7 +101,7 @@ $("#display1").on('click', function (e){
                 scrollTop: 0
             }, 'slow');
     }
-    
+    counter--;
 });
 
 
@@ -69,7 +110,7 @@ function hideShowLinks() {
         var self = $(this);
         if (self.hasClass('toppy')) {
             toppyDiv = k;
-            console.log(toppyDiv);
+           // console.log(toppyDiv);
             /*if (k == 0) {
                 console.log('hide prev');
                 $('#display1').show();
@@ -87,7 +128,7 @@ function hideShowLinks() {
 //hideShowLinks();
 
 $(window).scroll(function () {
-    console.log("hi from scroll = " +  $(window).scrollTop());
+    //console.log("hi from scroll = " +  $(window).scrollTop());
     //if($(window).scrollTop() > sValues[toppyDiv])
     hideShowLinks();
 });
